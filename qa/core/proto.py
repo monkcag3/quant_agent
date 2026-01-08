f4057501868096fe0ef8f134a820dafd245dd2e9
monkcag3 <142334041+monkcag3@users.noreply.github.com>
2026-01-08 20:23:16 +0800

    Update proto.py

--- qa/core/proto.py
+++ qa/core/proto.py
@@ -19,6 +19,33 @@ class Tick(LittleEndianStructure):
         ('bid_volume' , c_uint64*10),
     ]
 
+class Bar(LittleEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ('datetime'   , c_uint64),
+        ('symbol'     , c_char*12),
+        ('open'       , c_double),
+        ('high'       , c_double),
+        ('low'        , c_double),
+        ('close'      , c_double),
+        ('volume'     ,c_uint64),
+        ('amount'     , c_double),
+    ]
+
+class Order(LittleEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ('orderid'   , c_char*128),
+        ('type'      , c_char*12),  # limit or market
+        ('direction' , c_char*12),  # buy or sell
+        ('symbol'    , c_char*12),
+        ('price'     , c_double),
+        ('volume'    , c_int),
+        ('traded'    , c_int),
+        ('status'    , c_char*24),
+        ('datetime'  , c_int64),
+    ]
+
 class Account(LittleEndianStructure):
     _pack_ = 1
     _fields_ = [
