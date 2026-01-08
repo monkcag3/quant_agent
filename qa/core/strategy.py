bd93a7b033031f9437277ad75fd85f20e383864f
monkcag3 <142334041+monkcag3@users.noreply.github.com>
2026-01-08 17:13:20 +0800

    Strategy中增加本地账户snapshot

--- qa/core/strategy.py
+++ qa/core/strategy.py
@@ -1,9 +1,13 @@
 
+from qa.core.account import QAccount
+
 
 class Strategy:
     def __init__(self):
         self._md_adaptor = None
         self._td_adaptor = None
+        self.event_engine = None
+        self.acc = QAccount()
 
     def on_init(self):
         pass
@@ -14,6 +18,13 @@ class Strategy:
     def register_broker(self, md_adaptor, td_adaptor):
         self._md_adaptor = md_adaptor
         self._td_adaptor = td_adaptor
+
+    def register_event_engine(self, event_engine):
+        self.event_engine = event_engine
+        event_engine.register('account', self.acc.on_rtn_account)
+        event_engine.register('position', self.acc.on_rtn_position)
+        event_engine.register('order', self.acc.on_rtn_order)
+        event_engine.register('trade', self.acc.on_rtn_trade)
 
     def subscribe(self, symbol):
         self._md_adaptor.subscribe(symbol)
