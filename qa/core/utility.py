11d55ab3b9392d11bfa82c61433ed9d105179f55
monkcag3 <142334041+monkcag3@users.noreply.github.com>
2026-01-08 19:21:05 +0800

    Update utility.py

--- qa/core/utility.py
+++ qa/core/utility.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from typing import Literal, overload, Callable
 from qa.core.proto import Bar, Tick
 
+
 class ArrayManager:
     """
     1. time series contianer of bar data
@@ -98,11 +99,416 @@ class ArrayManager:
     @overload
     def sma(self, n: int, array: Literal[True]) -> np.ndarray: ...
     def sma(self, n: int, array: bool = False) -> float | np.ndarray:
-        result: np.ndarray = talib.SMA(self.close, n)
+        result = talib.SMA(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def ema(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def ema(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def ema(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.EMA(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def kama(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def kama(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def kama(self, n:int, array: bool = False) -> float | np.ndarray:
+        result = talib.KAMA(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def wma(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def wma(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def wma(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.WMA(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def apo(self, fast_period: int, slow_period: int, matype: int = 0, array: Literal[False] = False) -> float: ...
+    @overload
+    def apo(self, fast_period: int, slow_period: int, matype: int = 0, *, array: Literal[True]) -> np.ndarray: ...
+    def apo(
+        self,
+        fast_period: int,
+        slow_period: int,
+        matype: int = 0,
+        array: bool = False
+    ) -> float | np.ndarray:
+        result = talib.APO(self.close, fast_period, slow_period, matype)
+        return result if array else result[-1]
+    
+    @overload
+    def cmo(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def cmo(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def cmo(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.CMO(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def mom(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def mom(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def mom(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.MOM(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def ppo(self, fast_period: int, slow_period: int, matype: int = 0, array: Literal[False] = False) -> float: ...
+    @overload
+    def ppo(self, fast_period: int, slow_period: int, matype: int = 0, *, array: Literal[True]) -> np.ndarray: ...
+    def ppo(
+        self,
+        fast_period: int,
+        slow_period: int,
+        matype: int = 0,
+        array: bool = False
+    ) -> float | np.ndarray:
+        result = talib.PPO(self.close, fast_period, slow_period, matype)
+        return result if array else result[-1]
+    
+    @overload
+    def roc(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def roc(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def roc(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.ROC(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def rocr(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def rocr(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def rocr(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.ROCR(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def rocp(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def rocp(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def rocp(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.ROCP(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def rocr100(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def rocr100(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def rocr100(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.ROCR100(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def trix(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def trix(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def trix(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.TRIX(self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def std(self, n: int, nbdev: int = 1, array: Literal[False] = False) -> float: ...
+    @overload
+    def std(self, n: int, nbdev: int = 1, *, array: Literal[True]) -> np.ndarray: ...
+    def std(self, n: int, nbdev: int = 1, array: bool = False) -> float | np.ndarray:
+        result = talib.STDDEV(self.close, n, nbdev)
+        return result if array else result[-1]
+    
+    @overload
+    def obv(self, array: Literal[False] = False) -> float: ...
+    @overload
+    def obv(self, array: Literal[True]) -> np.ndarray: ...
+    def obv(self, array: bool = False) -> float | np.ndarray:
+        result = talib.OBV(self.close, self.volume)
+        return result if array else result[-1]
+    
+    @overload
+    def cci(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def cci(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def cci(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.CCI(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def atr(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def atr(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def atr(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.ATR(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def natr(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def natr(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def natr(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.NATR(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+
+    @overload
+    def rsi(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def rsi(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def rsi(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.RSI(self.close, n)
+        return result if array else result[-1]
+
+    @overload
+    def macd(self, fast_period: int, slow_period: int, signal_period: int, array: Literal[False] = False) -> tuple[float, float, float]: ...
+    @overload
+    def macd(self, fast_period: int, slow_period: int, signal_period: int, array: Literal[True]) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+    def macd(
+        self,
+        fast_period: int,
+        slow_period: int,
+        signal_period: int,
+        array: bool = False
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray] | tuple[float, float, float]:
+        macd, signal, hist = talib.MACD(
+            self.close, fast_period, slow_period, signal_period
+        )
+        return (macd, signal, hist) if array else (macd[-1], signal[-1], hist[-1])
+
+    @overload
+    def adx(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def adx(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def adx(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.ADX(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+
+    @overload
+    def adxr(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def adxr(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def adxr(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.ADXR(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def dx(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def dx(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def dx(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.DX(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def minus_di(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def minus_di(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def minus_di(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.MINUS_DI(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def plus_di(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def plus_di(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def plus_di(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.PLUS_DI(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+
+    @overload
+    def willr(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def willr(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def willr(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.WILLR(self.high, self.low, self.close, n)
+        return result if array else result[-1]
+    
+    @overload
+    def ultosc(self, time_period1: int = 7, time_period2: int = 14, time_period3: int = 28, array: Literal[False] = False) -> float: ...
+    @overload
+    def ultosc(self, time_period1: int = 7, time_period2: int = 14, time_period3: int = 28, *, array: Literal[True]) -> np.ndarray: ...
+    def ultosc(
+        self,
+        time_period1: int = 7,
+        time_period2: int = 14,
+        time_period3: int = 28,
+        array: bool = False
+    ) -> float | np.ndarray:
+        result = talib.ULTOSC(self.high, self.low, self.close, time_period1, time_period2, time_period3)
+        return result if array else result[-1]
+    
+    @overload
+    def trange(self, array: Literal[False] = False) -> float: ...
+    @overload
+    def trange(self, array: Literal[True]) -> np.ndarray: ...
+    def trange(self, array: bool = False) -> float | np.ndarray:
+        result = talib.TRANGE(self.high, self.low, self.close)
+        return result if array else result[-1]
+    
+    @overload
+    def boll(self, n: int, dev: float, array: Literal[False] = False) -> tuple[float, float]: ...
+    @overload
+    def boll(self, n: int, dev: float, array: Literal[True]) -> tuple[np.ndarray, np.ndarray]: ...
+    def boll(
+        self,
+        n: int,
+        dev: float,
+        array: bool = False
+    ) -> tuple[np.ndarray, np.ndarray] | tuple[float, float]:
+        mid_array = talib.SMA(self.close, n)
+        std_array = talib.STDDEV(self.close, n, 1)
+
         if array:
-            return result
-        result: float = result[-1]
-        return result
+            up_array = mid_array + std_array * dev
+            down_array = mid_array - std_array * dev
+            return up_array, down_array
+        else:
+            mid = mid_array[-1]
+            std = std_array[-1]
+            up = mid + std * dev
+            down = mid - std * dev
+            return up, down
+        
+    @overload
+    def keltner(self, n: int, dev: float, array: Literal[False] = False) -> tuple[float, float]: ...
+    @overload
+    def keltner(self, n: int, dev: float, array: Literal[True]) -> tuple[np.ndarray, np.ndarray]: ...
+    def keltner(
+        self,
+        n: int,
+        dev: float,
+        array: bool = False
+    ) -> tuple[np.ndarray, np.ndarray] | tuple[float, float]:
+        mid_array = talib.SMA(self.close, n)
+        atr_array = talib.ATR(self.high, self.low, self.close, n)
+
+        if array:
+            up_array = mid_array + atr_array * dev
+            down_array = mid_array - atr_array * dev
+            return up_array, down_array
+        else:
+            mid = mid_array[-1]
+            atr = atr_array[-1]
+            up = mid + atr * dev
+            down = mid - atr * dev
+            return up, down
+        
+    @overload
+    def donchian(self, n: int, array: Literal[False] = False) -> tuple[float, float]: ...
+    @overload
+    def donchian(self, n: int, array: Literal[True]) -> tuple[np.ndarray, np.ndarray]: ...
+    def donchian(
+        self, n: int, array: bool = False
+    ) -> tuple[np.ndarray, np.ndarray] | tuple[float, float]:
+        up = talib.MAX(self.high, n)
+        down = talib.MIN(self.low, n)
+        return (up, down) if array else (up[-1], down[-1])
+    
+    @overload
+    def aroon(self, n: int, array: Literal[False] = False) -> tuple[float, float]: ...
+    @overload
+    def aroon(self, n: int, array: Literal[True]) -> tuple[np.ndarray, np.ndarray]: ...
+    def aroon(
+        self,
+        n: int,
+        array: bool = False
+    ) -> tuple[np.ndarray, np.ndarray] | tuple[float, float]:
+        aroon_down, aroon_up = talib.AROON(self.high, self.low, n)
+        return (aroon_up, aroon_down) if array else (aroon_up[-1], aroon_down[-1])
+
+    @overload
+    def aroonosc(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def aroonosc(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def aroonosc(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.AROONOSC(self.high, self.low, n)
+        return result if array else result[-1]
+    
+    @overload
+    def minus_dm(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def minus_dm(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def minus_dm(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.MINUS_DM(self.high, self.low, n)
+        return result if array else result[-1]
+
+    @overload
+    def plus_dm(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def plus_dm(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def plus_dm(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.PLUS_DM(self.high, self.low, n)
+        return result if array else result[-1]
+
+    @overload
+    def mfi(self, n: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def mfi(self, n: int, array: Literal[True]) -> np.ndarray: ...
+    def mfi(self, n: int, array: bool = False) -> float | np.ndarray:
+        result = talib.MFI(self.high, self.low, self.close, self.volume, n)
+        return result if array else result[-1]
+    
+    @overload
+    def ad(self, array: Literal[False] = False) -> float: ...
+    @overload
+    def ad(self, array: Literal[True]) -> np.ndarray: ...
+    def ad(self, array: bool = False) -> float | np.ndarray:
+        result = talib.AD(self.high, self.low, self.close, self.volume)
+        return result if array else result[-1]
+    
+    @overload
+    def adosc(self, fast_period: int, slow_period: int, array: Literal[False] = False) -> float: ...
+    @overload
+    def adosc(self, fast_period: int, slow_period: int, array: Literal[True]) -> np.ndarray: ...
+    def adosc(
+        self,
+        fast_period: int,
+        slow_period: int,
+        array: bool = False
+    ) -> float | np.ndarray:
+        result = talib.ADOSC(self.high, self.low, self.close, self.volume, fast_period, slow_period)
+        return result if array else result[-1]
+    
+    @overload
+    def bop(self, array: Literal[False] = False) -> float: ...
+    @overload
+    def bop(self, array: Literal[True]) -> np.ndarray: ...
+    def bop(self, array: bool = False) -> float | np.ndarray:
+        result = talib.BOP(self.open, self.high, self.low, self.close)
+        return result if array else result[-1]
+    
+    @overload
+    def stoch(self, fastk_period: int, slowk_period: int, slowk_matype: int, slowd_period: int, slowd_matype: int, array: Literal[False] = False) -> tuple[float, float]: ...
+    @overload
+    def stoch(self, fastk_period: int, slowk_period: int, slowk_matype: int, slowd_period: int, slowd_matype: int, array: Literal[True]) -> tuple[np.ndarray, np.ndarray]: ...
+    def stoch(
+        self,
+        fastk_period: int,
+        slowk_period: int,
+        slowk_matype: int,
+        slowd_period: int,
+        slowd_matype: int,
+        array: bool = False
+    ) -> tuple[float, float] | tuple[np.ndarray, np.ndarray]:
+        k, d = talib.STOCH(
+            self.high,
+            self.low,
+            self.close,
+            fastk_period,
+            slowk_period,
+            slowk_matype,    # type: ignore
+            slowd_period,
+            slowd_matype     # type: ignore
+        )
+        return (k, d) if array else (k[-1], d[-1])
+
+    @overload
+    def sar(self, acceleration: float, maximum: float, array: Literal[False] = False) -> float: ...
+    @overload
+    def sar(self, acceleration: float, maximum: float, array: Literal[True]) -> np.ndarray: ...
+    def sar(self, acceleration: float, maximum: float, array: bool = False) -> float | np.ndarray:
+        result = talib.SAR(self.high, self.low, acceleration, maximum)
+        return result if array else result[-1]
+
 
 
 class BarUpdater:
@@ -110,7 +516,7 @@ class BarUpdater:
         self,
         am: ArrayManager,
         interval: str,
-        on_bar: Callable
+        on_bar: Callable = None
     ) -> None:
         self._am = am
         self._interval = int(interval[:-1])
@@ -210,6 +616,7 @@ class BarUpdater:
         self,
         tick: Tick
     ) -> None:
+        self._bar.datetime = tick.datetime
         if tick.high > self._bar.high:
             self._bar.high = tick.high
         if tick.low < self._bar.low:
