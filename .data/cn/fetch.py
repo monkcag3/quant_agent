eefc8236e5bdafd32aca8c3a495d75fe0d0b46b1
monkcag3 <142334041+monkcag3@users.noreply.github.com>
2026-04-17 16:49:32 +0800

    Update fetch.py

--- .data/cn/fetch.py
+++ .data/cn/fetch.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from pathlib import Path
 import duckdb
 import pandas as pd
+from tqdm  import tqdm 
 
 # ==================== 【必须改这里】 ====================
 QMT_BIN_PATH = r"D:/opt/QMT/bin.x64"  # 你的QMT路径
@@ -34,12 +35,10 @@ class XtQuantDuckDBStorage:
     def get_db(self, exchange, symbol):
         path = self.get_db_path(exchange, symbol)
         print(path)
-        key = str(path)
-        # if key not in self.conn_cache:
+
         con = duckdb.connect(str(path))
         self._create_tables(con)
-        #     self.conn_cache[key] = con
-        # return self.conn_cache[key]
+
         return con
 
     def _create_tables(self, con):
@@ -54,94 +53,17 @@ class XtQuantDuckDBStorage:
     def parse_symbol_exchange(self, symbol):
         return "cn", symbol
 
-    def download_one(self, symbol, period, start=None, end=None, incrementally=True):
-        if end is None:
-            end = datetime.now().strftime("%Y%m%d%H%M%S")[:8]
-        if start is None:
-            if period == "1d":
-                start = "19700101"
-            elif period in ["1m", "5m"]:
-                start = "19700101"
-            elif period == "tick":
-                start = (datetime.now()-timedelta(days=30)).strftime("%Y%m%d")
-            else:
-                start = "19700101"
 
-        try:
-            xtdata.download_history_data(symbol, period, start, end, incrementally)
-            df = xtdata.get_local_data([], [symbol], period, start, end, -1, 0)
-            df = df.unstack(0).dropna()
-            df.columns = ["open", "high", "low", "close", "volume", "amount"]
-            df = df.reset_index()
-            df["dt"] = pd.to_datetime(df["time"], unit="ms").dt.tz_localize(TZ_CHINA)
-            df = df.drop(columns=["time"])
-
-            exc, sym = self.parse_symbol_exchange(symbol)
-            con = self.get_db(exc, sym)
-            con.execute(f"INSERT OR REPLACE INTO {period} SELECT * FROM df")
-            print(f"✅ {symbol} | {period} | 写入 {len(df)} 行")
-        except Exception as e:
-            print(f"❌ {symbol} 失败: {str(e)}")
-
-    def download_all_stocks(self, stock_list, period, start=None, end=None):
-        """下载【全部A股】"""
-        print(f"📌 共获取到 {len(stock_list)} 只A股，开始下载...")
-
-        # for i, code in enumerate(stock_list[:2], 1):
-        #     print(f"\n[{i}/{len(stock_list)}] ", end="")
-        #     self.download_one(code, period, start, end, incrementally=True)
-        #     time.sleep(0.2)
-        try:
-            xtdata.download_history_data2(stock_list, period, start_time=start, end_time='20240416', callback=self.on_progress)
-        except Exception as e:
-            print(e)
-
-    def on_progress(self, data):
-        print(data)
-
-    def close_all(self):
-        for con in self.conn_cache.values():
-            con.close()
-
-# def on_progress(data):
-#     print(data)
-
-# ==================== 【一键下载全市场】 ====================
-if __name__ == "__main__":
-    from tqdm  import tqdm 
+def save_to_duckdb(
+    stock_list: list,
+    period: str,
+    start_time: str,
+    end_time: str,
+) -> None:
     storage = XtQuantDuckDBStorage()
+    for symbol in tqdm (stock_list):
 
-    stock_list = xtdata.get_stock_list_in_sector("沪深A股")
-    period = '5m'
-
-    # 下载所有股票 日线（上市至今）
-    # storage.download_all_stocks(stock_list, period="1d", start="19700101")
-
-    # 下载所有股票 5分钟（最近1年）
-    # storage.download_all_stocks(stock_list[:2], period=period, start="19700101")
-    # for symbol in tqdm (stock_list):
-    #     xtdata.download_history_data(symbol, period, "19700101", "20260417", True)
-
-    # 下载所有股票 1分钟（最近1年）
-    # storage.download_all_stocks(period="1m", start="19700101")
-
-    # 下载所有股票 Tick（最近30天）
-    # storage.download_all_stocks(period="tick")
-
-    # #002110.SZ
-    # # print(stock_list)
-    # # print(stock_list.index('002110.SZ'))
-    # # stock_idx = stock_list.index('002110.SZ') - 1
-    # # stock_list = stock_list[stock_idx:]
-    # # symbol = '000151.SZ'
-
-    # print(stock_list[:2])
-    # items = xtdata.get_local_data([], stock_list=stock_list[:2], period=period, start_time="19700101", end_time='20260417',)
-    # print(items)
-
-    for symbol in tqdm (stock_list[:338]):
-
-        items = xtdata.get_local_data([], stock_list=[symbol], period=period, start_time="19700101", end_time='20260417',)
+        items = xtdata.get_local_data([], stock_list=[symbol], period=period, start_time=start_time, end_time=end_time,)
 
         exc, sym = storage.parse_symbol_exchange(symbol)
         con: duckdb.DuckDBPyConnection = storage.get_db(exc, sym)
@@ -153,3 +75,91 @@ if __name__ == "__main__":
             con.execute(f"INSERT OR REPLACE INTO bar_{period} SELECT dt_tz, open, high, low, close, volume, amount FROM df")
             print(f"✅ {symbol} | {period} | 写入 {len(df)} 行")
             con.close()
+
+
+def single_download_and_save(
+    stock_list: list[str],
+    period: str,
+    start: str,
+    end: str,
+) -> None:
+    for symbol in tqdm(stock_list):
+        xtdata.download_history_data(symbol, period, start, end, True)
+    save_to_duckdb(stock_list, period, start, end)
+
+
+def download_and_save(
+    start: str,
+    end: str,
+):
+    stock_list = xtdata.get_stock_list_in_sector("沪深A股")
+
+    # 历史日线
+    single_download_and_save(stock_list, '1d', start, end)
+
+    # 历史5min线
+    single_download_and_save(stock_list, '5m', start, end)
+
+    # 历史1min线
+    single_download_and_save(stock_list, '1m', start, end)
+
+    # 历史tick
+    single_download_and_save(stock_list, 'tick', start, end)
+
+
+
+def get_full_history():
+    """下载1d,1m,5m历史数据
+    1.先使用xtquant下载
+    2.xtquant中1m、5m只支持最近一年数据；更久远数据使用baostock获取
+    **xtquant和baostock中volume单位不一样：xtquant采用手数，baostock采用股数**
+    """
+
+    now = datetime.now().strftime("%Y%m%d")
+    download_and_save('19700101', now)
+
+
+
+def daily_update():
+    """每日数据更新"""
+
+    now = datetime.now()
+    yes = now - timedelta(days=1)
+    now = now.strftime("%Y%m%d")
+    yes = yes.strftime("%Y%m%d")
+
+    download_and_save(yes, now)
+
+
+# ==================== 【一键下载全市场】 ====================
+if __name__ == "__main__":
+    from tqdm  import tqdm 
+    # storage = XtQuantDuckDBStorage()
+
+    # stock_list = xtdata.get_stock_list_in_sector("沪深A股")
+    # now = datetime.now().strftime("%Y%m%d")
+
+    # # 下载所有股票 日线（上市至今）
+    # period = '1d'
+    # for symbol in tqdm (stock_list):
+    #     xtdata.download_history_data(symbol, period, "19700101", now, True)
+    # save_to_duckdb(stock_list, period, '19700101', now)
+
+    # # 下载所有股票 5分钟（最近1年）
+    # period = '5m'
+    # for symbol in tqdm (stock_list):
+    #     xtdata.download_history_data(symbol, period, "19700101", now, True)
+    # save_to_duckdb(stock_list, period, '19700101', now)
+
+    # # 下载所有股票 1分钟（最近1年）
+    # period = '1m'
+    # for symbol in tqdm (stock_list):
+    #     xtdata.download_history_data(symbol, period, "19700101", now, True)
+    # save_to_duckdb(stock_list, period, '19700101', now)
+
+    # # 下载所有股票 Tick（最近30天）
+    # period = 'tick'
+    # for symbol in tqdm (stock_list):
+    #     xtdata.download_history_data(symbol, period, "19700101", now, True)
+    # save_to_duckdb(stock_list, period, '19700101', now)
+    daily_update()
